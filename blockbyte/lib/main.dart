f58f14a7f8e32408c6311a2a6393a327b55e8d7d
Mike Schälchli <mike.schaelchli@earlybyte.ch>
2019-06-21 23:10:26 +0200

    Icons added

--- blockbyte/lib/main.dart
+++ blockbyte/lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'helper/api.dart';
 
 void main() => runApp(MyApp());
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
